48a31104ead1da6b260d5251c1aa71aa78cccd13
Greyven <greyven@gmail.com>
2018-09-20 17:08:13 +0200

    Create homepage controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="homepage", methods={"GET"})
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function homeAction()
+    {
+        return $this->render('home.html.twig');
+    }
+}
