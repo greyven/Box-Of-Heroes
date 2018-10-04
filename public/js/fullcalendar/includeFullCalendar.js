9080bb28505ef9b44ec5ac2324c6663f07ef254a
Greyven <greyven@gmail.com>
2018-10-04 12:23:03 +0200

    Separate Javascript from twig view

---
+++ public/js/fullcalendar/includeFullCalendar.js
@@ -0,0 +1,41 @@
+$(function ()
+{
+    $('#calendar-holder').fullCalendar(
+        {
+            locale: 'fr',
+            header:
+                {
+                    left: 'prev, next, today',
+                    center: 'title',
+                    right: 'month, agendaWeek, agendaDay'
+                },
+            businessHours:
+                {
+                    start: '09:00',
+                    end: '18:00',
+                    dow: [1, 2, 3, 4, 5]
+                },
+            defaultView: 'month',
+            lazyFetching: true,
+            navLinks: true,
+            selectable: true,
+            editable: true,
+            eventDurationEditable: true,
+            // events: '/box-of-heroes/public/booking/getBookings',
+            eventSources:
+                [
+                    {
+                        url: '/box-of-heroes/public/booking/getBookings',
+                        color: '#ef9857',
+                        textColor: 'black',
+                        type: 'GET',
+                        data: {
+                            filters: {}
+                        },
+                        error: function () {
+                            // alert('There was an error while fetching FullCalendar!');
+                        }
+                    }
+                ]
+        });
+});
