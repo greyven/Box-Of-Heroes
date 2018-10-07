06f7756f1e55ef49906b982525ef32079be7fd13
Greyven <greyven@gmail.com>
2018-10-07 15:51:14 +0200

    Home become Index

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin()
+    {
+        return $this->render('Admin/index.html.twig');
+    }
+
+}
